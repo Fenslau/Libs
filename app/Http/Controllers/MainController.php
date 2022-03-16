3f96954f9ed8b14a3fd1f8ba1b3511f6ccb4bffa
Fenslau <c01@bk.ru>
2022-03-16 20:39:38 +0700

    Пустой проект

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function main() {
+
+
+      $items = array();
+      return view('home', ['items' => $items]);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title-block', 'Test')
+@section('description-block', '')
+
+@section('content')
+
+<div class="container">
+  <div class="row">
+    <h2>Test</h2>
+  </div>
+</div>
+
+@endsection
